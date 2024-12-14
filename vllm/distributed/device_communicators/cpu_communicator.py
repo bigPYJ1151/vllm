5eaf79644526291d62588177b6986bd1144b57ad
jiang1.li <jiang1.li@intel.com>
2024-12-14 12:38:40 +0000

    add new shm_ccl
    Signed-off-by: jiang1.li <jiang1.li@intel.com>

--- vllm/distributed/device_communicators/cpu_communicator.py
+++ vllm/distributed/device_communicators/cpu_communicator.py
@@ -1,4 +1,6 @@
+import os
 from typing import List
+
 import torch
 import torch.distributed as dist
 from torch.distributed import ProcessGroup
@@ -17,8 +19,22 @@ class CpuCommunicator:
         self.world_size = dist.get_world_size(self.group)
         self.rank = dist.get_rank(self.group)
 
+        self._init_cpu_shm()
+
+    def _init_cpu_shm(self):
+        instance_name = os.environ["VLLM_INSTANCE_ID"]
+        group_name = f"{instance_name}-cpushm"
+        torch.ops._C.init_shm_manager(
+            group_name,
+            self.world_size,
+            self.rank,
+        )
+        torch.distributed.barrier(self.group)
+        torch.ops._C.join_shm_manager(group_name, )
+        torch.distributed.barrier(self.group)
+
     def all_reduce(self, input_: torch.Tensor) -> torch.Tensor:
-        torch.ops._C.shm_allreduce(input_, self.rank)
+        torch.ops._C.shm_allreduce(input_)
         return input_
 
     def gather(self,
@@ -29,15 +45,17 @@ class CpuCommunicator:
                dim: int = -1):
         # Allocate output tensor.
         if rank_in_group == dst:
-            gather_list = [torch.empty_like(input_) for _ in range(self.world_size)]
+            gather_list = [
+                torch.empty_like(input_) for _ in range(self.world_size)
+            ]
         else:
             gather_list = None
         # Gather.
-        torch.ops._C.shm_gather(input_, gather_list, ranks[dst],
-                        self.rank)
+        torch.ops._C.shm_gather(input_, gather_list, ranks[dst])
 
         if rank_in_group == dst:
             output_tensor = torch.cat(gather_list, dim=dim)
         else:
             output_tensor = None
+
         return output_tensor
