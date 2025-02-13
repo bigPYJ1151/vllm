4e4110566d9672e4aec4feff6fef6b750ac6a704
jiang1.li <jiang1.li@intel.com>
2025-02-13 02:27:41 +0000

    adapt new mp executor for thread binding

--- vllm/worker/cpu_worker.py
+++ vllm/worker/cpu_worker.py
@@ -201,6 +201,9 @@ class CPUWorker(LocalOrDistributedWorkerBase):
         else:
             self.profiler = None
 
+        # Note: For adapting mp executor which may use asyncio eventloop for the driver
+        self.runtime_thread_bind = True if self.rank == 0 else False
+
     def start_profile(self):
         if self.profiler is None:
             raise RuntimeError("Profiler is not enabled.")
@@ -214,7 +217,7 @@ class CPUWorker(LocalOrDistributedWorkerBase):
     def init_device(self) -> None:
         if self.local_omp_cpuid != "all":
             ret = torch.ops._C_utils.init_cpu_threads_env(self.local_omp_cpuid)
-            if ret:
+            if not self.runtime_thread_bind and ret:
                 logger.info(ret)
         os.environ["OMP_NUM_THREADS"] = str(torch.get_num_threads())
 
@@ -343,6 +346,13 @@ class CPUWorker(LocalOrDistributedWorkerBase):
         self,
         worker_input: WorkerInput,
     ) -> None:
+        if self.runtime_thread_bind:
+            self.runtime_thread_bind = False
+            if self.local_omp_cpuid != "all":
+                ret = torch.ops._C_utils.init_cpu_threads_env(self.local_omp_cpuid)
+                if self.runtime_thread_bind and ret:
+                    logger.info(ret)
+
         if (worker_input.blocks_to_copy is not None
                 and worker_input.blocks_to_copy.numel() > 0):
             self.cache_engine[worker_input.virtual_engine].copy(
